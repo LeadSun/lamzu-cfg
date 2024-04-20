95567c744317bd2103e825c34a8a717be9d683ff
LeadSun <163475548+LeadSun@users.noreply.github.com>
2024-04-20 15:22:00 +1000

    Add a binrw wrapper to append a checksum

--- src/device/atlantis/checksum.rs
+++ src/device/atlantis/checksum.rs
@@ -1,3 +1,5 @@
+use binrw::{binrw, BinRead, BinWrite};
+use std::fmt;
 use std::io::{Read, Seek, SeekFrom, Write};
 
 /// Wraps a reader / writer to add the calculation of a checksum.
@@ -59,4 +61,57 @@ pub fn compute_checksum(buf: &[u8]) -> u8 {
     }
 
     checksum
+}
+
+pub trait Checksummable: for<'a> BinRead<Args<'a> = ()> + for<'a> BinWrite<Args<'a> = ()> {}
+
+impl<T: for<'a> BinRead<Args<'a> = ()> + for<'a> BinWrite<Args<'a> = ()>> Checksummable for T {}
+
+/// Wraps an object to add a calculated checksum to the end.
+#[binrw]
+#[brw(big, stream = s, map_stream = Checksum::new)]
+pub struct Checksummed<T: Checksummable> {
+    inner: T,
+
+    #[br(temp, assert(s.checksum() == 0, "Bad checksum"))]
+    #[bw(calc(s.checksum()))]
+    _checksum: u8,
+}
+
+impl<T: Checksummable> Checksummed<T> {
+    pub fn new(inner: T) -> Self {
+        Self { inner }
+    }
+
+    pub fn into_inner(self) -> T {
+        self.inner
+    }
+}
+
+impl<T: Checksummable> From<T> for Checksummed<T> {
+    fn from(inner: T) -> Self {
+        Self { inner }
+    }
+}
+
+impl<T: Checksummable + Default> Default for Checksummed<T> {
+    fn default() -> Self {
+        Self {
+            inner: T::default(),
+        }
+    }
+}
+
+impl<T: Checksummable + Clone> Clone for Checksummed<T> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+        }
+    }
+}
+
+impl<T: Checksummable + fmt::Debug> fmt::Debug for Checksummed<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        self.inner.fmt(f)
+    }
 }
