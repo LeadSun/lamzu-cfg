f30c801f20b35a9f9dc9180a36010e20c3468ee1
LeadSun <163475548+LeadSun@users.noreply.github.com>
2024-05-13 07:11:11 +1000

    Change profile reader to buffer all bytes read
    Binrw uses a lot of rewinds when deserializing, which clears the reader
    buffer each time, causing unnecessary read requests. Change buffering to
    store all bytes from the start so rewinds don't trigger read requests.

--- src/device/atlantis/profile_rw.rs
+++ src/device/atlantis/profile_rw.rs
@@ -12,7 +12,7 @@ pub struct ProfileReader<'a> {
     /// HID device to read from.
     device: &'a HidDevice,
 
-    /// Buffered bytes read from `device` starting at `position`.
+    /// All buffered bytes read from `device`.
     buf: Vec<u8>,
 
     /// The origin address where data will start being read from.
@@ -35,56 +35,51 @@ impl<'a> ProfileReader<'a> {
 
 impl<'a> Read for ProfileReader<'a> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        if self.buf.is_empty() {
-            // Don't read past the end of the data.
-            let len = (DATA_END - (self.address + self.position)).min(10);
-            if len == 0 {
+        assert!(self.position <= self.buf.len());
+
+        // Need more data.
+        if self.position + buf.len() > self.buf.len() {
+            // Read as much data as possible in one go (max 10 bytes).
+            let req_len = (DATA_END - (self.address + self.position)).min(10);
+            if req_len == 0 {
                 return Ok(0);
             }
 
-            self.buf = make_request(
+            let new_bytes = make_request(
                 self.device,
-                &StandardReport::read_profile_data(self.address + self.position, len),
+                &StandardReport::read_profile_data(self.address + self.buf.len(), req_len),
             )
             .and_then(|response| response.into_data())
             .map_err(|error| io::Error::new(io::ErrorKind::Other, error))?;
+            self.buf.extend_from_slice(&new_bytes);
         }
 
-        let clone_bytes = buf.len().min(self.buf.len());
-        buf.clone_from_slice(&self.buf[..clone_bytes]);
-        self.buf.drain(..clone_bytes);
-        self.position += clone_bytes;
-        Ok(clone_bytes)
+        let len = buf.len().min(self.buf.len() - self.position);
+        buf[..len].clone_from_slice(&self.buf[self.position..(self.position + len)]);
+        self.position += len;
+
+        Ok(len)
     }
 }
 
 impl<'a> Seek for ProfileReader<'a> {
     fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
-        match pos {
-            SeekFrom::Start(from_start) => {
-                self.buf.clear();
-                self.position = from_start as usize;
-            }
-            SeekFrom::End(from_end) => {
-                self.buf.clear();
-                self.position = DATA_END
-                    .checked_add_signed(from_end as isize)
-                    .ok_or(io::Error::from(io::ErrorKind::InvalidInput))?
-            }
-            SeekFrom::Current(from_current) => {
-                // Attempt to retain some buffer contents.
-                if from_current < 0 || (from_current as usize) >= self.buf.len() {
-                    self.buf.clear();
-                } else if from_current > 0 {
-                    self.buf.drain(..(from_current as usize));
-                }
+        self.position = match pos {
+            SeekFrom::Start(from_start) => from_start as usize,
+            SeekFrom::End(from_end) => DATA_END
+                .checked_add_signed(from_end as isize)
+                .ok_or(io::Error::from(io::ErrorKind::InvalidInput))?,
+            SeekFrom::Current(from_current) => self
+                .position
+                .checked_add_signed(from_current as isize)
+                .ok_or(io::Error::from(io::ErrorKind::InvalidInput))?,
+        };
 
-                self.position = self
-                    .position
-                    .checked_add_signed(from_current as isize)
-                    .ok_or(io::Error::from(io::ErrorKind::InvalidInput))?
-            }
+        if self.position > self.buf.len() {
+            // Assumes that skipped data is not needed.
+            self.buf.resize(self.position, 0);
         }
+
         Ok(self.position as u64)
     }
 }
